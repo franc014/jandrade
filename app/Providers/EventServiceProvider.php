bd997c2831b1bee7785bb871dc50b85acab9da58
JUan Andrade <jfandradea@gmail.com>
2015-11-06 19:17:54 -0500

    SendEmail event handler

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use App\Events\SendEmail;
-use App\Listeners\Events\SendEmailHandler;
+use App\Listeners\SendEmailHandler;
 use Illuminate\Contracts\Events\Dispatcher as DispatcherContract;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
